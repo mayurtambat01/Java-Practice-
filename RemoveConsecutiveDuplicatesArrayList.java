45791e1eb74034ae55e15c35c402aa661908dba6
mayurtambat01 <mayurtambat01@gmail.com>
2024-05-27 15:18:50 +0530

    update ArrayList

--- RemoveConsecutiveDuplicatesArrayList.java
+++ RemoveConsecutiveDuplicatesArrayList.java
@@ -6,7 +6,7 @@ public class RemoveConsecutiveDuplicatesArrayList {
 
         ArrayList<Integer> result = new ArrayList<>();
         result.add(arr[0]);
-        for(int i=0;i < arr.length;i++){
+        for(int i=1;i < arr.length;i++){
             if(arr[i] != arr[i - 1]){
                 result.add(arr[i]);
             }
@@ -19,7 +19,7 @@ public class RemoveConsecutiveDuplicatesArrayList {
         int arr[]={10,10,10,20,20,30,40,10};
         ArrayList<Integer> result = removeDuplicates(arr);
         for(int i=0;i < result.size();i++){
-            System.out.println(result.get(i));
+            System.out.print(result.get(i)+" ");
         }
     }
 
